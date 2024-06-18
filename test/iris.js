b46c1cb0dbbd0ba72ee5fc0005068ff980678357
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-18 16:45:47 +0700

    Added MarkedJS and tested it

--- test/iris.js
+++ test/iris.js
@@ -3,6 +3,10 @@
 class Iris {
     constructor(src) {
         this.src = src;
+
+        // this.tokens = this.lexer(this.src);
+        // this.ast = this.tokens(this.src, this.tokens);
+        // this.out = this.output(this.src, this.tokens, this.ast);
     }
 
     lexer() {
