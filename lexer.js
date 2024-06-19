4a71864b1cc239d118f2980b009e1e279f2d8733
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-19 09:20:26 +0700

    Update lexer.js

---
+++ lexer.js
@@ -0,0 +1,80 @@
+export const Tokens = {
+  Content: "Content",
+  Plus: "Plus",
+  Minus: "Minus",
+  Greater: "Greater",
+  Eol: "Eol",
+  Eof: "Eof",
+};
+
+export class Token {
+  constructor(type, content) {
+    this.type = type;
+    this.content = content;
+  }
+}
+
+export class Lexer {
+  constructor(source = "") {
+    this.source = source;
+    this.tokens = [];
+    this.current = 0;
+  }
+
+  peek() {
+    if (this.current >= this.source.length) return "\0";
+    return this.source[this.current];
+  }
+
+  peekNext() {
+    if (this.current + 1 >= this.source.length) return "\0";
+    return this.source[this.current + 1];
+  }
+
+  advance() {
+    if (this.current >= this.source.length) return "\0";
+    return this.source[this.current++];
+  }
+
+  scan() {
+    const content = (char) => {
+      let result = char; // Starting char
+      while (this.peek() !== "\0" && this.peek() !== "\n") {
+        result += this.advance();
+      }
+      return result;
+    };
+
+    while (this.peek() !== "\0") {
+      let char = this.advance();
+
+      switch (char) {
+        case "\n": {
+          this.tokens.push(new Token(Tokens["Eol"], "\n"));
+          break;
+        }
+        case " ":
+          break;
+        case "+": {
+          this.tokens.push(new Token(Tokens["Plus"], "+"));
+          break;
+        }
+        case "-": {
+          this.tokens.push(new Token(Tokens["Minus"], "-"));
+          break;
+        }
+        case ">": {
+          this.tokens.push(new Token(Tokens["Greater"], ">"));
+          break;
+        }
+        default: {
+          let c = content(char);
+          this.tokens.push(new Token(Tokens["Content"], c));
+        }
+      }
+    }
+
+    this.tokens.push(new Token(Tokens["Eof"], "\0"));
+    return this.tokens;
+  }
+}
