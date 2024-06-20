e8ad4990419a628b4b394b7b61446fece2048629
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-06-20 09:53:22 +0700

    Update codegen.js

--- codegen.js
+++ codegen.js
@@ -14,23 +14,24 @@ export class CodeGen {
 
   push(node, list) {
     if (node.type === Types["Section"]) {
-      this.jsList.push(`function ${node.name}(){`);
+      list.push(`function ${node.name}(){`);
       node.body.forEach((child) => {
-        this.push(child);
+        this.push(child, list);
       });
-      this.jsList.push(`}`);
+      list.push(`}`);
     } else if (node.type === Types["Text"]) {
-      this.jsList.push(`text("${node.content}");`);
+      list.push(`text("${node.content}");`);
     } else if (node.type === Types["Choice"]) {
       let choiceList = [];
       node.body.forEach((child) => {
-        choiceList.push(this.push(child));
+        this.push(child, choiceList);
       });
       node.body = choiceList;
-      console.log(choiceList);
-      this.jsList.push(`choice("${node.content}");`);
+      this.analise(node.body);
+      console.log(node);
+      list.push(`choice("${node.content}");`);
     } else if (node.type === Types["Diversion"]) {
-      this.jsList.push(
+      list.push(
         `if(${node.section}){return ${node.section}();}else{console.error('Error: Section ${node.section} is undefined')}`,
       );
     }
@@ -38,10 +39,17 @@ export class CodeGen {
 
   analise(list) {
     for (let i = list.length - 1; i >= 0; i -= 1) {
+      if (list[i - 1] && list[i - 1].startsWith("choice") && list[i].startsWith("choice")) {
+        list[i - 1] =
+          list[i - 1].substr(0, list[i - 1].length - 2) +
+          `,()=>{${list[i]}}` +
+          list[i - 1].substr(list[i - 1].length - 2);
+        list[i] = "";
+      }
       if (list[i - 1] && list[i - 1].startsWith("text") && list[i] != "}") {
         list[i - 1] =
           list[i - 1].substr(0, list[i - 1].length - 2) +
-          `,()=>{ ${list[i]} }` +
+          `,()=>{${list[i]}}` +
           list[i - 1].substr(list[i - 1].length - 2);
         list[i] = "";
       }
